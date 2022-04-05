c344e62b489ad2b1203ad07697316152691fc746
RenenKantor <renenkantor@campus.technion.ac.il>
2022-04-05 20:34:51 +0300

    Extracted methods for easier build

--- lib/draw_screen.dart
+++ lib/draw_screen.dart
@@ -22,106 +22,137 @@ class DrawState extends State<DrawerScreen> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-            padding: const EdgeInsets.only(left: 60.0, right: 60.0),
-            decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(20.0), color: const Color.fromRGBO(255, 215, 0, 1)),
-            child: Padding(
+          padding: const EdgeInsets.only(left: 60.0, right: 60.0),
+          decoration:
+              BoxDecoration(borderRadius: BorderRadius.circular(20.0), color: const Color.fromRGBO(255, 215, 0, 1)),
+          child: Padding(
               padding: const EdgeInsets.all(4.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                          icon: const Icon(Icons.line_weight),
-                          onPressed: () {
-                            setState(() {
-                              if (selectedMenu == MenuSelection.strokeWidth) {
-                                displayMenu = !displayMenu;
-                              }
-                              selectedMenu = MenuSelection.strokeWidth;
-                            });
-                          }),
-                      IconButton(
-                          icon: const Icon(Icons.color_lens_outlined),
-                          onPressed: () {
-                            setState(() {
-                              if (selectedMenu == MenuSelection.brushColor) {
-                                displayMenu = !displayMenu;
-                              }
-                              selectedMenu = MenuSelection.brushColor;
-                            });
-                          }),
-                      IconButton(
-                          icon: const Icon(Icons.settings_outlined),
-                          onPressed: () {
-                            setState(() {
-                              if (selectedMenu == MenuSelection.settingMenu) {
-                                displayMenu = !displayMenu;
-                              }
-                              selectedMenu = MenuSelection.settingMenu;
-                            });
-                          })
-                    ],
+                    children: <Widget>[createStrokeWidget(), createColorsWidget(), createSettingsWidget()],
                   ),
-                  AnimatedOpacity(
-                      opacity: displayMenu ? 1 : 0,
-                      duration: const Duration(milliseconds: 600),
-                      child: Visibility(
-                        child: (selectedMenu == MenuSelection.brushColor)
-                            ? Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getColoredCircleList())
-                            : (selectedMenu == MenuSelection.strokeWidth)
-                                ? Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getWidthCircleList())
-                                : Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getSettingsList()),
-                        visible: displayMenu,
-                      ))
+                  createMenuWidget()
                 ],
-              ),
-            )),
+              )),
+        ),
       ),
-      body: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() {
-            RenderBox renderBox = context.findRenderObject() as RenderBox;
-            points.add(DrawingPoint(
-                pointLocation: renderBox.globalToLocal(details.globalPosition),
-                paint: Paint()
-                  ..strokeCap = StrokeCap.round
-                  ..isAntiAlias = true
-                  ..color = selectedColor.withOpacity(opacity)
-                  ..strokeWidth = strokeWidth));
-          });
-        },
-        onPanStart: (details) {
-          setState(() {
-            RenderBox renderBox = context.findRenderObject() as RenderBox;
-            points.add(DrawingPoint(
-                pointLocation: renderBox.globalToLocal(details.globalPosition),
-                paint: Paint()
-                  ..strokeCap = StrokeCap.round
-                  ..isAntiAlias = true
-                  ..color = selectedColor.withOpacity(opacity)
-                  ..strokeWidth = strokeWidth));
-          });
-        },
-        onPanEnd: (details) {
-          setState(() {
-            points.add(DrawingPoint(pointLocation: const Offset(-1, -1), paint: Paint()));
-            for (var p in points) {
-              p.printPoint();
-            }
-            debugPrint("*****************************");
-          });
-        },
-        child: CustomPaint(
-          size: Size.infinite,
-          painter: DrawingPainter(
-            pointsList: points,
-          ),
+      body: drawUserInput(context),
+    );
+  }
+
+  GestureDetector drawUserInput(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (details) {
+        setState(() {
+          RenderBox renderBox = context.findRenderObject() as RenderBox;
+          points.add(DrawingPoint(
+              pointLocation: renderBox.globalToLocal(details.globalPosition),
+              paint: Paint()
+                ..strokeCap = StrokeCap.round
+                ..isAntiAlias = true
+                ..color = selectedColor.withOpacity(opacity)
+                ..strokeWidth = strokeWidth));
+        });
+      },
+      onPanStart: (details) {
+        setState(() {
+          RenderBox renderBox = context.findRenderObject() as RenderBox;
+          points.add(DrawingPoint(
+              pointLocation: renderBox.globalToLocal(details.globalPosition),
+              paint: Paint()
+                ..strokeCap = StrokeCap.round
+                ..isAntiAlias = true
+                ..color = selectedColor.withOpacity(opacity)
+                ..strokeWidth = strokeWidth));
+        });
+      },
+      onPanEnd: (details) {
+        setState(() {
+          points.add(DrawingPoint(pointLocation: const Offset(-1, -1), paint: Paint()));
+          for (var p in points) {
+            p.printPoint();
+          }
+          debugPrint("************** NEW LINE ***************");
+        });
+      },
+      child: CustomPaint(
+        size: Size.infinite,
+        painter: DrawingPainter(
+          pointsList: points,
         ),
       ),
     );
+  }
+
+  AnimatedOpacity createMenuWidget() {
+    return AnimatedOpacity(
+        opacity: displayMenu ? 1 : 0,
+        duration: const Duration(milliseconds: 600),
+        child: Visibility(
+          child: selectCurrentMenu(),
+          visible: displayMenu,
+        ));
+  }
+
+  Row selectCurrentMenu() {
+    if (selectedMenu == MenuSelection.strokeWidth) {
+      return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getWidthCircleList());
+    }
+    if (selectedMenu == MenuSelection.brushColor) {
+      return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getColoredCircleList());
+    }
+    if (selectedMenu == MenuSelection.settingMenu) {
+      return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: getSettingsList());
+    }
+    throw Exception("Invalid menu selected");
+  }
+
+  IconButton createStrokeWidget() {
+    return IconButton(
+        icon: const Icon(Icons.line_weight),
+        onPressed: () {
+          setState(() {
+            if (selectedMenu == MenuSelection.strokeWidth) {
+              displayMenu = !displayMenu;
+            } else {
+              displayMenu = true;
+            }
+            selectedMenu = MenuSelection.strokeWidth;
+          });
+        });
+  }
+
+  IconButton createColorsWidget() {
+    return IconButton(
+        icon: const Icon(Icons.color_lens_outlined),
+        onPressed: () {
+          setState(() {
+            if (selectedMenu == MenuSelection.brushColor) {
+              displayMenu = !displayMenu;
+            } else {
+              displayMenu = true;
+            }
+            selectedMenu = MenuSelection.brushColor;
+          });
+        });
+  }
+
+  IconButton createSettingsWidget() {
+    return IconButton(
+        icon: const Icon(Icons.settings_outlined),
+        onPressed: () {
+          setState(() {
+            if (selectedMenu == MenuSelection.settingMenu) {
+              displayMenu = !displayMenu;
+            } else {
+              displayMenu = true;
+            }
+            selectedMenu = MenuSelection.settingMenu;
+          });
+        });
   }
 
   getColoredCircleList() {
