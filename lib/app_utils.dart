674ec26660db866ad835855a8dc345da44d6aa51
RenenKantor <renenkantor@campus.technion.ac.il>
2022-05-15 17:00:04 +0300

    Const correctness and  reformating

--- lib/app_utils.dart
+++ lib/app_utils.dart
@@ -1,18 +1,23 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 // 8cm/1000 = 8 * 10^-5 meters/tick left/right/up/down.
-enum MenuSelection { strokeWidth, brushColor, settingMenu }
-enum UploadFlag { readyForPulse, readingPulse, startDraw, reuploadLast, sendNumOfMoves }
-enum PointType { regular, dummyUp, dummyDown }
 enum RobotMove { right, left, up, down, rightUp, rightDown, leftUp, leftDown, servoUp, servoDown }
+enum UploadFlag { readyForPulse, readingPulse, startDraw, reuploadLast, sendNumOfMoves }
 enum PulseStatus { nextPulse, reuploadPulse, finishedPulses }
+enum MenuSelection { strokeWidth, brushColor, settingMenu }
+enum PointType { regular, dummyUp, dummyDown }
 
+const Offset dummyOffset = Offset(-1, -1);
 const double a4Width = 297;
 const double a4Height = 210;
 const double pixelToMM = 0.26458333;
-const Offset dummyOffset = Offset(-1, -1);
 const int pulseCapacity = 500;
+
+final DatabaseReference numOfMovesRef = FirebaseDatabase.instance.ref("NumOfMoves");
+final DatabaseReference movesRef = FirebaseDatabase.instance.ref("RobotMoves");
+final DatabaseReference flagRef = FirebaseDatabase.instance.ref("Flag");
 
 class CompMove {
   final RobotMove move;
@@ -31,6 +36,22 @@ class DrawingPoint {
   }
 }
 
-final DatabaseReference movesRef = FirebaseDatabase.instance.ref("RobotMoves");
-final DatabaseReference flagRef = FirebaseDatabase.instance.ref("Flag");
-final DatabaseReference numOfMovesRef = FirebaseDatabase.instance.ref("NumOfMoves");
+class DrawingPainter extends CustomPainter {
+  const DrawingPainter({required this.pointsList});
+  final List<DrawingPoint> pointsList;
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      final Offset curLocation = pointsList[i].pointLocation;
+      final Offset nextLocation = pointsList[i + 1].pointLocation;
+      if (curLocation != dummyOffset && nextLocation != dummyOffset) {
+        canvas.drawLine(curLocation, nextLocation, pointsList[i].paint);
+      } else if (curLocation != dummyOffset && nextLocation == dummyOffset) {
+        canvas.drawPoints(
+            PointMode.points, [curLocation, Offset(curLocation.dx + 0.1, curLocation.dy + 0.1)], pointsList[i].paint);
+      }
+    }
+  }
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'draw_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'draw_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
