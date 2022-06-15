8776ab30aa9ac3bb3c2462616f251f06345f94dd
RonD101 <ron.dahan@campus.technion.ac.il>
2022-06-15 21:48:35 +0300

    Fix TCP protocal

--- arduino/paintBot/paintBot.ino
+++ arduino/paintBot/paintBot.ino
@@ -15,29 +15,23 @@
 #define WIFI_PASSWORD ""
 //#define WIFI_SSID "RonDiPhone"
 //#define WIFI_PASSWORD "12345678"
-
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyAIVdnB_mJA6_ZV5G9ctjWO7aQRLJk_DjQ"
 // Insert RTDB URLefine the RTDB URL */
 #define DATABASE_URL "https://paintbot-a1067-default-rtdb.firebaseio.com/"
-
 //#define WEB_DEBUG
 #define SERIAL_DEBUG
-
 #define LEFT_MOTOR_ENABLE_PIN 35
 #define LEFT_MOTOR_STEP_PIN 25
 #define LEFT_MOTOR_DIR_PIN 26
-
 #define RIGHT_MOTOR_ENABLE_PIN 32
 #define RIGHT_MOTOR_STEP_PIN 13
 #define RIGHT_MOTOR_DIR_PIN 12
-
 #define SERVO_PIN 33
-
 #define LEFT_SENSOR_PIN 14
 #define DOWN_SENSOR_PIN 27
-
 #define LED_PIN 2
+#define PAYLOAD_SIZE 500
 
 #ifdef WEB_DEBUG
   AsyncWebServer server(80);
@@ -51,27 +45,26 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
-int intValue;
-float floatValue;
-bool signupOK = false;
 
 // This bools should only be true if we chose different configuration for the motors
 // (like if the motor is upsidedown)
 bool shouldSwitchLeftMotorDir = true;
 bool shouldSwitchRightMotorDir = false;
 
-bool shouldStepMotor1 = false;
-bool shouldStepMotor2 = false;
+bool shouldStepLeftMotor = false;
+bool shouldStepRightMotor = false;
 
 int counter = 0;
 int HomeLeftState = 0;
 int HomeDownState = 0;
+int timeBetweenReads = 3000;
 bool initLeft = true;
 bool initDown = true;
 bool waitForRealseButton = false;
 int flag;
 int NumOfMoves;
 int* movesArray;
+bool finishedDraw = true;
 
 void setup() {
   // Motot 1
@@ -112,175 +105,167 @@ void setup() {
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
 
-  digitalWrite(LED_PIN, HIGH);
-
   /* Assign the api key (required) */
   config.api_key = API_KEY;
 
   /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
+  while (!Firebase.signUp(&config, &auth, "", "")){
     Serial.println(config.signer.signupError.message.c_str());
   }
+  Serial.println("FireBase sign up connected");
 
   /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback; 
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  digitalWrite(LED_PIN, HIGH);
+
    #ifdef WEB_DEBUG
      WebSerial.begin(&server);
      server.begin();
    #endif
-  delay(2000);
 }
 
 void loop() {
-  if (Firebase.ready() && signupOK)
+  if (Firebase.ready())
     digitalWrite(LED_PIN, HIGH);
-  else
+  else {
     digitalWrite(LED_PIN, LOW);
-    
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 3000 || sendDataPrevMillis == 0)) {
-    sendDataPrevMillis = millis();
-    if (Firebase.RTDB.getInt(&fbdo, "/Flag")) {
-      debugPrint("Got flag = ");
-      debugPrintln(fbdo.intData());
-      flag = fbdo.intData();
-      if (flag == 0)
-        return;
-    } else {
+    return;
+  }
+
+  if (millis() - sendDataPrevMillis < timeBetweenReads && sendDataPrevMillis != 0)
+    return;
+  sendDataPrevMillis = millis();
+  if (Firebase.RTDB.getInt(&fbdo, "/Flag")) {
+    debugPrint("Got flag = ");
+    debugPrintln(fbdo.intData());
+    flag = fbdo.intData();
+    if (flag == 0)
+      return;
+  } else {
       printDebugErrors();
-    }
-    if (flag == 4) {
-        if (Firebase.RTDB.getInt(&fbdo, "/NumOfMoves")) {
+      return;
+  }
+  if (flag == 4) {
+      timeBetweenReads = 500;
+      if (Firebase.RTDB.getInt(&fbdo, "/NumOfMoves")) {
         debugPrint("Got number of moves = ");
         debugPrintln(fbdo.intData());
         NumOfMoves = fbdo.intData();
         movesArray = new int[NumOfMoves];
-        } else {
-          printDebugErrors();
-        }
-    }
-    if (flag == 2) {
-      debugPrint("Amount of pulse got = ");
-      debugPrintln(counter);
-      counter = 0;
-      debugPrint("Start Draw ");
-      debugPrint(NumOfMoves);
-      debugPrintln(" points!");
-      for (int i = 0; i < NumOfMoves; i = i + 2) {
-        int len = movesArray[i];
-        int elem = movesArray[i + 1];
-        debugPrint("i = ");
-        debugPrintln(i);
-        debugPrint("NumOfMoves = ");
-        debugPrintln(NumOfMoves);
-        debugPrint("len = ");
-        debugPrintln(len);
-        debugPrint("elem = ");
-        debugPrintln(elem);
-        for (int j = 0; j < len; j++) {
-          if (elem == 0)
-              stepRight();
-          else if (elem == 1)
-              stepLeft();
-          else if (elem == 2)
-              stepUp();
-          else if (elem == 3)
-              stepDown();
-          else if (elem == 4)
-              stepRightUp();
-          else if (elem == 5)
-              stepRightDown();
-          else if (elem == 6)
-              stepLeftUp();
-          else if (elem == 7)
-              stepLeftDown();
-          else if (elem == 8) {
-              delay(300);
-              myservo.write(180); // servo up
-              delay(300);
-          }
-          else if (elem == 9) {
-              delay(300);
-              myservo.write(50); // servo down
-              delay(300);
-          }
-          else if (elem == 10) {
-              goHome();
-          }
-          else
-              debugPrintln("Error got unexpected robot move");
-        }
+      } else {
+        printDebugErrors();
+        return;
       }
-      debugPrintln("End Draw!");
-//      delete[] movesArray;
+  }
+  if (flag == 2 && finishedDraw) {
+    finishedDraw = false;
+    debugPrint("Amount of pulse got = ");
+    debugPrintln(counter);
+    counter = 0;
+    debugPrint("Start Draw ");
+    debugPrint(NumOfMoves);
+    debugPrintln(" points!");
+    for (int i = 0; i < NumOfMoves; i = i + 2) {
+      int len = movesArray[i];
+      int elem = movesArray[i + 1];
+      for (int j = 0; j < len; j++) {
+        if (elem == 0)
+            stepRight();
+        else if (elem == 1)
+            stepLeft();
+        else if (elem == 2)
+            stepUp();
+        else if (elem == 3)
+            stepDown();
+        else if (elem == 4)
+            stepRightUp();
+        else if (elem == 5)
+            stepRightDown();
+        else if (elem == 6)
+            stepLeftUp();
+        else if (elem == 7)
+            stepLeftDown();
+        else if (elem == 8) {
+            delay(300);
+            myservo.write(180); // servo up
+            delay(300);
+        }
+        else if (elem == 9) {
+            delay(300);
+            myservo.write(50); // servo down
+            delay(300);
+        }
+        else if (elem == 10) {
+            goHome();
+        }
+        else
+            debugPrintln("Error got unexpected robot move");
+      }
     }
-    if (flag == 1) {
-      if (Firebase.RTDB.getArray(&fbdo, "/RobotMoves")) {
-        debugPrintln("Get array ok");
-        FirebaseJsonArray arr = fbdo.jsonArray();
-        FirebaseJsonData currValue;
-        debugPrint("Array size: ");
+    debugPrintln("End Draw!");
+    timeBetweenReads = 3000;
+    delete[] movesArray;
+  }
+  if (flag == 1) {
+    if (Firebase.RTDB.getArray(&fbdo, "/RobotMoves")) {
+      debugPrintln("Get array ok");
+      FirebaseJsonArray arr = fbdo.jsonArray();
+      FirebaseJsonData currValue;
+      debugPrint("Array size: ");
+      debugPrintln(arr.size());
+      arr.get(currValue, 0);
+      int amountOfElemInArray = currValue.to<int>();
+      if (amountOfElemInArray + 1 != arr.size()) {
+        bool writtenError = false;
+        while(!writtenError) {
+          writtenError = Firebase.RTDB.setInt(&fbdo, "/Flag", 3);
+        }
+        debugPrintln("Set Flag to 3 because of array sizes that doesn't match");
+        debugPrint("first element: ");
+        debugPrintln(amountOfElemInArray);
+        debugPrint("arr size: ");
         debugPrintln(arr.size());
-        arr.get(currValue, 0);
-        int amountOfElemInArray = currValue.to<int>();
-        if (amountOfElemInArray + 1 != arr.size()) {
-          if (Firebase.RTDB.setInt(&fbdo, "/Flag", 3)){
-            debugPrintln("Set Flag to 3 because of array sizes that doesn't match");
-            debugPrint("first element: ");
-            debugPrintln(amountOfElemInArray);
-            debugPrint("arr size: ");
-            debugPrintln(arr.size());
-            return;
-          }
-          else {
-            printDebugErrors();
-          }
-        } else {
-          for (size_t i = 0; i < arr.size(); i++)
-            {
-              arr.get(currValue, i + 1);
-              movesArray[1000 * counter + i] = currValue.to<int>();
-            }
-          debugPrintln("Finished load!");
-          counter += 1;
-        }
+        return;
       }
-      else {
-        printDebugErrors();
-        if (Firebase.RTDB.setInt(&fbdo, "/Flag", 3)){
-          debugPrintln("Set Flag to 3");
-          return;
-        }
-        else {
-          printDebugErrors();
-        }
+      for (size_t i = 0; i < arr.size() - 1; i++)
+      {
+        arr.get(currValue, i + 1);
+        movesArray[PAYLOAD_SIZE * counter + i] = currValue.to<int>();
       }
-    }
-   if (flag == -1) { // For debug
-      if (Firebase.RTDB.setString(&fbdo, "/IP", WiFi.localIP().toString())){
-        debugPrint("IP: ");
-        debugPrintln(WiFi.localIP());
+      debugPrintln("Finished load!");
+      counter += 1;
+    } else {
+      debugPrintln("Weired shit man");
+      printDebugErrors();
+      bool writtenError = false;
+      while(!writtenError) {
+        writtenError = Firebase.RTDB.setInt(&fbdo, "/Flag", 3);
       }
-      else {
-        printDebugErrors();
-      }
+      debugPrintln("Set Flag to 3");
+      return;
    }
-   if (Firebase.RTDB.setInt(&fbdo, "/Flag", 0)){
-      debugPrintln("Set Flag to zero");
+ }
+ if (flag == -1) { // For debug
+    if (Firebase.RTDB.setString(&fbdo, "/IP", WiFi.localIP().toString())){
+      debugPrint("IP: ");
+      debugPrintln(WiFi.localIP());
     }
     else {
       printDebugErrors();
     }
-  }
+ }
+ bool writtenZero = false;
+ while (!writtenZero) {
+    writtenZero = Firebase.RTDB.setInt(&fbdo, "/Flag", 0);
+ }
+ debugPrintln("Set Flag to zero");
+ finishedDraw = true;
 }
 
 void printDebugErrors() {
@@ -318,14 +303,16 @@ void goHome() {
   debugPrintln("Start go home");
   HomeDownState = digitalRead(DOWN_SENSOR_PIN);
   delay(10);
-  while (HomeDownState == HIGH) {
+  while (HomeDownState == HIGH)
+  {
     stepDown();
     HomeDownState = digitalRead(DOWN_SENSOR_PIN);
   }
   debugPrintln("Finish go down");
   HomeLeftState = digitalRead(LEFT_SENSOR_PIN);
   delay(10);
-  while (HomeLeftState == HIGH) {
+  while (HomeLeftState == HIGH)
+  {
     stepLeft();
     HomeLeftState = digitalRead(LEFT_SENSOR_PIN);
   }
@@ -333,76 +320,76 @@ void goHome() {
 }
 
 void stepMotors() {
-  if (shouldStepMotor1) {
+  if (shouldStepLeftMotor) {
     digitalWrite(LEFT_MOTOR_STEP_PIN, HIGH);
   }
-  if (shouldStepMotor2) {
+  if (shouldStepRightMotor) {
     digitalWrite(RIGHT_MOTOR_STEP_PIN, HIGH);
   }
-  if (shouldStepMotor1) {
+  if (shouldStepLeftMotor) {
     digitalWrite(LEFT_MOTOR_STEP_PIN, LOW);
   }
-  if (shouldStepMotor2) {
+  if (shouldStepRightMotor) {
     digitalWrite(RIGHT_MOTOR_STEP_PIN, LOW);
   }
   delay(1);
 }
 
 void stepLeftDown() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = false;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = false;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? HIGH : LOW);
   stepMotors();
 }
 
 void stepRightUp() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = false;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = false;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? LOW : HIGH);
   stepMotors();
 }
 
 void stepRightDown() {
-  shouldStepMotor1 = false;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = false;
+  shouldStepRightMotor = true;
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? LOW : HIGH);
   stepMotors();
 }
 
 void stepLeftUp() {
-  shouldStepMotor1 = false;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = false;
+  shouldStepRightMotor = true;
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? HIGH : LOW);
   stepMotors();
 }
 
 void stepLeft() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = true;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? HIGH : LOW);
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? HIGH : LOW);
   stepMotors();
 }
 
 void stepRight() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = true;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? LOW : HIGH);
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? LOW : HIGH);
   stepMotors();
 }
 
 void stepDown() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = true;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? HIGH : LOW);
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? LOW : HIGH);
   stepMotors();
 }
 
 void stepUp() {
-  shouldStepMotor1 = true;
-  shouldStepMotor2 = true;
+  shouldStepLeftMotor = true;
+  shouldStepRightMotor = true;
   digitalWrite(LEFT_MOTOR_DIR_PIN, shouldSwitchLeftMotorDir ? LOW : HIGH);
   digitalWrite(RIGHT_MOTOR_DIR_PIN, shouldSwitchRightMotorDir ? HIGH : LOW);
   stepMotors();
